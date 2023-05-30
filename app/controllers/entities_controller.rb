21cccc70bf5a83ae99181ee26bdd6465553ce971
anas599 <anas599@yahoo.com>
2023-05-31 00:46:54 +0300

    remove unused code

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,23 +1,17 @@
 class EntitiesController < ApplicationController
   before_action :set_entity, only: %i[show edit update destroy]
-
-  # GET /entities or /entities.json
   def index
     @entities = Entity.where(user_id: current_user.id)
   end
 
-  # GET /entities/1 or /entities/1.json
   def show; end
 
-  # GET /entities/new
   def new
     @entity = Entity.new
   end
 
-  # GET /entities/1/edit
   def edit; end
 
-  # POST /entities or /entities.json
   def create
     @entity = Entity.new(entity_params)
 
@@ -34,7 +28,6 @@ class EntitiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /entities/1 or /entities/1.json
   def update
     respond_to do |format|
       if @entity.update(entity_params)
@@ -47,7 +40,6 @@ class EntitiesController < ApplicationController
     end
   end
 
-  # DELETE /entities/1 or /entities/1.json
   def destroy
     @entity.destroy
 
@@ -59,12 +51,10 @@ class EntitiesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_entity
     @entity = Entity.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def entity_params
     params.require(:entity).permit(:name, :amount, :user_id, :group_id)
   end
