ac3fe247dbe595d1097dba3caeb91772c41fd167
anas599 <anas599@yahoo.com>
2023-05-31 00:39:32 +0300

    fix linters rubocop

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,6 @@ class GroupsController < ApplicationController
   # GET /groups or /groups.json
   def index
     @groups = Group.where(user_id: current_user)
-
   end
 
   # GET /groups/1 or /groups/1.json
