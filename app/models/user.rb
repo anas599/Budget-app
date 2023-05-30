f59950d0067305edc9a7313c00e9a3361aa430ba
anas599 <anas599@yahoo.com>
2023-05-31 00:45:43 +0300

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  # validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30 }
 end
